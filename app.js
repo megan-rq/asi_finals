a4c39a4d201fec83dddfb03b406dbd037d6c4fa4
CeeDizon <94109584+CeeDizon@users.noreply.github.com>
2021-11-11 18:26:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // CS - 401     6ASI    Activity
 // November 11 2021
 
+
 const bodyParser = require("body-parser");
 const express = require("express");
 const admin = require("firebase-admin");
@@ -12,14 +13,17 @@ const app = express();
 app.engine("html", require("ejs").renderFile);
 app.use(express.static("static"));
 
+//get login
 app.get("/login", function (req, res) {
   	res.render("login.html");
 });
 
+//get signup
 app.get("/signup", function (req, res) {
   	res.render("signup.html");
 });
 
+//get profile
 app.get("/profile", function (req, res) {
     res.render("profile.html");
 });
